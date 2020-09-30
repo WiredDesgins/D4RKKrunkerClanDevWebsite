743fb1ce23476350adb6b570d6c02d608000cad4
WiredDesgins <72008357+WiredDesgins@users.noreply.github.com>
2020-09-30 10:24:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
  * @copyright : https://markis.dev
  */
  
-#Enable this to have Error Logging
-#Click on OAuth Link to log in
-# Enabling error display
-#error_reporting(E_ALL);
-#ini_set('display_errors', 1);
+Enable this to have Error Logging
+Click on OAuth Link to log in
+ Enabling error display
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
  
 
 # Including all the required scripts for demo
@@ -28,6 +28,17 @@ require __DIR__ . "/discord.php";
       
   </head>
   <body>
+     <title>Demo - Discord Oauth</title>
+     <h1>A Simple Working Demo of the Script </h2>
+     <h2 style="color:red; font-weight:900;"> LOGIN WITH THE LINK BELOW TO SEE IT WORK! </h3>
+     <h1> User Details :</h1>
+     <p> Name : <?php echo $_SESSION['username'] . '#'. $_SESSION['discrim']; ?></p>
+     <p> ID : <?php echo $_SESSION['user_id']; ?></p>
+     <p> Profile Picture : <img src="https://cdn.discordapp.com/avatars/<?php $extention = is_animated($_SESSION['user_avatar']); echo $_SESSION['user_id'] . "/" . $_SESSION['user_avatar'] . $extention; ?>" /></p>
+     <p> Response : <?php echo json_encode($_SESSION['user']); ?></p>
+     <br />
+     <h1> User Guilds :</h1>
+     <p> <?php echo json_encode($_SESSION['guilds']); ?></p>
      <h3 style="display:inline;"><a href="
      <?php echo url("760766489367281705", "https://d4rkdev.herokuapp.com/login.php", "identify guilds"); ?>
      <button class="button button1">Login to discord</button>
